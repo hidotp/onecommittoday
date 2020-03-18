ab09c5e8342fb75c5a9cae5ac53a177194d571a7
hidotp <38942179+hidotp@users.noreply.github.com>
2020-03-19 00:03:34 +0100

    implement core structure of masonry list

---
+++ frontend/src/components/MasonryItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>name: {{this.name}}</h1>
+    <h2>kudos: {{this.kudos}}</h2>
+    <p>{{this.desc}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'masonry-item',
+  props: {
+    name: String,
+    kudos: Number,
+    story: String
+    // iconLink: String,
+  }
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <masonry-item name="This is home" v-bind:kudos="42" story="eakalwldlawl"></masonry-item>
+    <masonry-list></masonry-list>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MasonryItem from '@/components/MasonryItem'
+import MasonryList from '@/components/MasonryList'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MasonryItem,
+    MasonryList
   }
 }
 </script>
