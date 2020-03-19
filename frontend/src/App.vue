8de745f8bea8b70c1dd9d76507ed98a3fb47316b
hidotp <38942179+hidotp@users.noreply.github.com>
2020-03-19 13:26:08 +0100

    Export nav into new file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <web-nav></web-nav>
     <router-view/>
   </div>
 </template>
+
+<script>
+import WebNav from '@/components/WebNav'
+
+export default {
+  name: 'App',
+  components: {
+    WebNav
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ frontend/src/components/WebNav.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
