e477395f2eead00e2b46362e445c8dcdad734679
hidotp <38942179+hidotp@users.noreply.github.com>
2020-03-19 12:28:32 +0100

    add dummy implementation of grid layout

--- frontend/src/components/MasonryList.vue
+++ frontend/src/components/MasonryList.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <masonry-item v-for="fed in feed" v-bind:key="fed.name" v-bind:name="fed.name" v-bind:story="fed.story" v-bind:kudos="fed.kudos"></masonry-item>
+    <masonry
+    :cols="3"
+    :gutter="30"
+    >
+      <masonry-item v-for="fed in tempFeed" v-bind:key="fed.name" v-bind:name="fed.name" v-bind:story="fed.story" v-bind:kudos="fed.kudos"></masonry-item>
+    </masonry>
   </div>
 </template>
 
@@ -17,7 +22,44 @@ export default {
   },
   data () {
     return {
-      feed: []
+      feed: [],
+      tempFeed: [
+        {
+          name: 'test1',
+          kudos: 23,
+          story: 'this is the story for 1 bla bla bla'
+        },
+        {
+          name: 'test2',
+          kudos: 23,
+          story: 'this is the story for 2 bla bla bla'
+        },
+        {
+          name: 'test3',
+          kudos: 23,
+          story: 'this is the story for 3 bla bla bla'
+        },
+        {
+          name: 'test4',
+          kudos: 23,
+          story: 'this is the story for 4 bla bla bla'
+        },
+        {
+          name: 'test5',
+          kudos: 23,
+          story: 'this is the story for 5 bla bla bla'
+        },
+        {
+          name: 'test6',
+          kudos: 23,
+          story: 'this is the story for 6 bla bla bla'
+        },
+        {
+          name: 'test7',
+          kudos: 23,
+          story: 'this is the story for 7 bla bla bla'
+        }
+      ]
     }
   },
   methods: {
@@ -25,15 +67,24 @@ export default {
       const path = API_URL + 'feed'
       axios.get(path)
         .then((res) => {
-          this.feed = res.data.feed
+          this.feed += res.data.feed
         })
         .catch((error) => {
           console.log(error)
         })
+    },
+    scroll () {
+      window.onscroll = () => {
+        const bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
+        if (bottomOfWindow) {
+          this.tempFeed = this.tempFeed.concat(this.tempFeed)
+        }
+      }
     }
   },
   created () {
     this.getFeed()
+    this.scroll()
   }
 }
 </script>
