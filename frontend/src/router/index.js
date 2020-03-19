9e5928e0ba3aba90b9d1c0086d59c98f89296df6
hidotp <38942179+hidotp@users.noreply.github.com>
2020-03-19 11:40:53 +0100

    Add masonry

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueMasonry from 'vue-masonry-css'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
+Vue.use(VueMasonry)
 
 const routes = [
   {
