aac5489d9f10d4356e33cb34e5303fb226b0e3ee
hidotp <38942179+hidotp@users.noreply.github.com>
2020-03-21 16:50:36 +0100

    update masonry fitment

--- frontend/src/components/MasonryItem.vue
+++ frontend/src/components/MasonryItem.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="wrapper">
-      <div class="one">
-        <img class="grid-icon" src="../assets/logo.png"/>
+      <div class="grid-icon centered">
+        <img class="icon" src="../assets/logo.png"/>
       </div>
-      <div class="two">
-        <h1 class="grid-name">name: {{this.name}}</h1>
+      <div class="grid-name centered">
+        <h1 id="name-element" class="name">{{this.name}}</h1>
       </div>
-      <div class="three">
-        <h2 class="grid-streak">streak: {{this.streak}}</h2>
+      <div class="grid-streak centered">
+        <div class="streak">streak: {{this.streak}}</div>
       </div>
-      <div class="four">
-        <p class="grid-kudos">{{this.story}}</p>
+      <div class="grid-story centered">
+        <p class="story">{{this.story}}</p>
       </div>
-      <div class="five">
-        <h2 class="grid-kudos">kudos: {{this.internalKudos}}</h2>
+      <div class="grid-kudos centered">
+        <div class="kudos">kudos: {{this.internalKudos}}</div>
         <button :disabled="submitted" class="grid-button" v-on:click="addKudos()">add kudos</button>
       </div>
     </div>
@@ -50,10 +50,13 @@ export default {
         .catch((error) => {
           console.log(error)
         })
+    },
+    updateFitment () {
     }
   },
   created () {
     this.internalKudos = this.kudos
+    window.addEventListener('resize', this.updateFitment)
   }
 }
 </script>
@@ -62,40 +65,44 @@ export default {
 
 .wrapper {
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 10px;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 5px;
   grid-auto-rows: minmax(100px, auto);
   padding: 5px;
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   border-radius: 5px;
+  margin: 0px 10px;
   margin-top: 10px;
 }
-.grid-icon {
-  width: 15vw;
+.icon {
+  width: 12vw;
+  min-width: 125px;
+  max-width: 200px;
 }
-.one {
-  background-color: brown;
-  grid-column: 1 / 2;
+.grid-icon {
+  grid-column: 1;
   grid-row: 1 / 3;
 }
-.two {
-  background-color: burlywood;
-  grid-column: 2 / 4;
+.grid-name {
+  grid-column: 2;
   grid-row: 1 / 2;
 }
-.three {
-  background-color: green;
-  grid-column: 2 / 4;
+.grid-streak {
+  grid-column: 2;
   grid-row: 2 / 3;
 }
-.four {
-  background-color: orchid;
-  grid-column: 1 / 4;
+.grid-story {
+  grid-column: 1 / 3;
   grid-row: 3 / 5;
 }
-.five {
-  background-color: orange;
-  grid-column: 1 / 4;
+.grid-kudos {
+  grid-column: 1 / 3;
   grid-row: 5;
+}
+.centered {
+  margin: auto;
+}
+.name {
+  font-size: 100%;
 }
 </style>
