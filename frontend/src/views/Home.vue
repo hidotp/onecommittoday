5179c341402b4ea639e4b9ecc5a47fb34dce3663
hidotp <38942179+hidotp@users.noreply.github.com>
2020-03-21 16:49:50 +0100

    rm masonry item from home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <masonry-item name="This is home" v-bind:kudos="42" story="eakalwldlawl"></masonry-item>
     <masonry-list></masonry-list>
   </div>
 </template>
 
 <script>
-import MasonryItem from '@/components/MasonryItem'
 import MasonryList from '@/components/MasonryList'
 
 export default {
   name: 'Home',
   components: {
-    MasonryItem,
     MasonryList
   }
 }
