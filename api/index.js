4f815210b35a5406ced00df262783a41ad841699
schneefux <schneefux+commit@schneefux.xyz>
2020-03-22 10:44:48 +0100

    Implement GitHub OAuth + API

--- api/index.js
+++ api/index.js
@@ -5,9 +5,9 @@ const session = require('koa-session');
 const Router = require('koa-router');
 const Knex = require('knex');
 const passport = require('koa-passport');
-const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const cron = require('node-cron');
+const OAuth2Strategy = require('passport-oauth2');
 
 const PORT = parseInt(process.env.PORT || '') || 3001;
 const DB_URI = process.env.DB_URI;
@@ -15,6 +15,8 @@ const SESSION_SECRET = process.env.SESSION_SECRET;
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const PUBLIC_HOST = process.env.PUBLIC_HOST || ('localhost:' + PORT);
+const URL_LOGIN_SUCCESS = process.env.URL_LOGIN_SUCCESS || 'http://localhost:8080';
+const URL_LOGIN_FAILURE = process.env.URL_LOGIN_FAILURE || 'http://localhost:8080';
 
 if (process.env.NODE_ENV == 'production' && DB_URI == undefined) {
   throw new Error('DB_URI must be set with production NODE_ENV');
@@ -42,13 +44,37 @@ passport.use(new OAuth2Strategy({
   tokenURL: 'https://github.com/login/oauth/access_token',
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  userProfileURL: 'https://github.com/api/v3/user',
   callbackURL: 'http://' + PUBLIC_HOST + '/auth/github/callback',
-}, (acccessToken, refreshToken, profile, done) => {
-  done(null, profile);
-  // knex('users') ...
+  scope: '', // grant read-only access to public information
+}, async (accessToken, unusedRefreshToken, unusedProfile, done) => {
+  const profileData = await getProfileData(accessToken);
+
+  const count = await knex('users')
+    .count('name', {as: 'count'})
+    .where('name', profileData.name) // TODO identify by id, not by name
+    .then(result => result[0].count);
+
+  const user = {
+    ...profileData,
+    access_token: accessToken,
+    story: '',
+    kudos: 0,
+  };
+
+  if (count == 0) {
+    await knex('users').insert(user);
+    console.log('registered ' + profileData.name);
+  } else {
+    // store new access token
+    await knex('users').update(user).where('name', profileData.name);
+    console.log('updated ' + profileData.name);
+  }
+
+  // put name into ctx.state.user
+  done(null, profileData.name);
 }));
 
+// serialize ctx.state.user <-> cookie
 passport.serializeUser((user, done) => {
   done(null, user);
 });
@@ -64,50 +90,86 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors({ origin: '*' }));
 
-async function fetchGithubActivity(username, from, to) {
-  const url = `https://github.com/${username}?tab=overview&from=${from}&to=${to}`;
-  const data = await fetch(url);
-  const $ = cheerio.load(await data.text());
-  const $days = $('rect.day');
+async function getProfileData(accessToken) {
+  const outbreak = '2020-03-11T00:00:00Z'; // the WHO officially declares the coronavirus outbreak to be a pandemic
+  const now = new Date().toISOString();
+  const query = `
+    query {
+      viewer {
+        login
+        avatarUrl
+        contributionsCollection(from: "${outbreak}", to: "${now}") {
+          contributionCalendar {
+            weeks {
+              contributionDays {
+                contributionCount
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
 
-  return $days.get().reduce((o, day) => {
-    const $day = $(day);
-    const date = $day.attr('data-date');
-    const count = parseInt($day.attr('data-count'), 10);
-    return { ...o, [date]: count };
-  }, {});
-}
+  const response = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  });
 
-async function fetchStreak(username) {
-  const from = '2020-03-11'; // the WHO officially declares the coronavirus outbreak to be a pandemic
-  const to = new Date().toISOString().substring(0, 10);
-  const activity = await fetchGithubActivity(username, from, to);
-  // response is sorted
-  const { streak } = [...Object.entries(activity)]
-    .filter(([date, commits]) => date >= from && date <= to)
-    .map(([date, commits]) => commits)
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.message);
+  }
+  const { data } = await response.json();
+
+  // data.viewer.contributionsCollection.contributionCalendar is sorted
+  const days = [].concat(...data.viewer.contributionsCollection.contributionCalendar.weeks.map(week => week.contributionDays.contributionCount));
+  const { streak } = days
     .reverse()
     // streak = count consecutive commits backwards from today
     // do not break streak if no commit for today yet (index == 0)
     .reduce(({ streak, end }, commits, index) => commits == 0 || end ? { streak, end: index > 0 } : { streak: streak + 1, end },
       { streak: 0, end: false });
-  return streak;
+
+  return {
+    name: data.viewer.login,
+    avatar_url: data.viewer.avatarUrl,
+    streak,
+  }
 }
 
-async function updateUserStreaks() {
-  const names = await knex('users').select('name');
-  console.log('updating streaks for ' + names.length + ' users');
-  await Promise.all(names.map(async ({ name }) =>
-    knex('users').update('streak', await fetchStreak(name)).where('name', name)
-  ));
+async function updateUsers() {
+  const accessTokens = await knex('users').select('access_token');
+  console.log('updating streaks for ' + accessTokens.length + ' users');
+  await Promise.all(accessTokens.map(async ({ access_token }) => {
+    try {
+      knex('users')
+        .update(await getProfileData(access_token))
+        .where('access_token', access_token)
+    } catch (error) {
+      console.error(error);
+    }
+  }));
 }
 
+// frontend redirects to this URL.
+// redirect the user to the GitHub login screen
 router.get('/auth/github', passport.authenticate('oauth2'));
 
-router.get('/auth/github/callback', passport.authenticate('oauth2'), async ctx => {
-  ctx.redirect('/'); // TODO
-});
+// then, GitHub redirects to this URL
+router.get('/auth/github/callback',
+  // set a cookie
+  passport.authenticate('oauth2', {
+    // redirect the user back to the frontend
+    successRedirect: URL_LOGIN_SUCCESS,
+    failureRedirect: URL_LOGIN_FAILURE,
+  }));
 
+// clear the cookie
 router.post('/logout', async ctx => ctx.logout());
 
 router.post('/:user/kudos', async ctx => {
@@ -119,6 +181,7 @@ router.post('/:user/kudos', async ctx => {
     .select('kudos')
     .where('name', name);
   ctx.body = kudos;
+  console.log('gave a kudo to ' + name);
 });
 
 router.post('/story', async ctx => {
@@ -133,32 +196,20 @@ router.post('/story', async ctx => {
   const story = ctx.request.body.story;
   const name = ctx.state.user;
 
-  const count = await knex('users')
-    .count('name', {as: 'count'})
-    .where('name', name)
-    .then(result => result[0].count);
+  await knex('users')
+    .update({ story })
+    .where('name', name);
 
-  if (count) {
-    await knex('users').update({ story });
-  } else {
-    await knex('users').insert({
-      name,
-      story,
-      kudos: 0,
-      streak: 0,
-    });
-  }
-
-  ctx.body = '';
+  ctx.body = await knex('users').where('name', name);
+  console.log('updated story for ' + name);
 });
 
 router.get('/feed', async ctx => {
   const { limit = 10, offset = 0 } = ctx.query;
-  const users = await knex('users')
+  ctx.body = await knex('users')
     .select('name', 'story', 'kudos', 'streak')
     .limit(Math.min(100, limit))
     .offset(offset);
-  ctx.body = users;
 });
 
 app.use(router.routes());
@@ -168,32 +219,23 @@ async function startup() {
   if (!await knex.schema.hasTable('users')) {
     await knex.schema.createTable('users', (table) => {
       table.string('name').notNullable().primary();
+      table.string('avatar_url').notNullable();
+      table.string('access_token').notNullable();
+      table.integer('streak').unsigned().notNullable();
       table.text('story').notNullable();
       table.bigInteger('kudos').notNullable();
-      table.integer('streak').unsigned().notNullable();
     });
 
     await knex('users').insert([
       // names are from https://gist.github.com/paulmillr/2657075
-      { name: 'andrew', story: 'I develop a Corona Virus dashboard', kudos: 1, streak: 1 },
-      { name: 'taylorotwell', story: 'I lost my job.', kudos: 0, streak: 5 },
-      { name: 'egoist', story: 'Since I can\'t go out, I spend my free time contributing to open source projects', kudos: 123, streak: 10 },
-      { name: 'ornicar', story: 'I am afraid of the future', kudos: 2, streak: 3 },
-      /*
-      ...(Array.from(Array(20)).map((_, n) =>
-        ({
-          name: 'test' + n,
-          story: 'Aut esse voluptates esse. Adipisci aut eos est consectetur voluptatem qui.',
-          kudos: Math.floor(Math.random() * 100),
-          streak: Math.floor(Math.random() * 30)
-        })
-      ))
-      */
+      { name: 'andrew', avatar_url: '', access_token: '', story: 'I develop a Corona Virus dashboard', kudos: 1, streak: 1 },
+      { name: 'taylorotwell', avatar_url: '', access_token: '', story: 'I lost my job.', kudos: 0, streak: 5 },
+      { name: 'egoist', avatar_url: '', access_token: '', story: 'Since I can\'t go out, I spend my free time contributing to open source projects', kudos: 123, streak: 10 },
+      { name: 'ornicar', avatar_url: '', access_token: '', story: 'I am afraid of the future', kudos: 2, streak: 3 },
     ])
   }
 
-  await updateUserStreaks();
-  cron.schedule('0 * * * *', updateUserStreaks); // every hour
+  cron.schedule('0 * * * *', updateUsers); // every hour
   app.listen(PORT || 3001, () => console.log(`Server running on http://localhost:${PORT}/`));
 }
 
