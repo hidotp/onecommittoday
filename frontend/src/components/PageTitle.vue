52833e24e8f56a3b11ff6dadee822860933da467
hidotp <38942179+hidotp@users.noreply.github.com>
2020-04-03 22:50:40 +0200

    add page title

---
+++ frontend/src/components/PageTitle.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="title">
+    a commit a day
+  </div>
+</template>
+
+<style scoped>
+.title {
+  padding-top: 14vh;
+  text-align: center;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
+    <page-title/>
     <masonry-list></masonry-list>
   </div>
 </template>
 
 <script>
 import MasonryList from '@/components/MasonryList'
+import PageTitle from '@/components/PageTitle'
 
 export default {
   name: 'Home',
   components: {
-    MasonryList
+    MasonryList,
+    PageTitle
   }
 }
 </script>
